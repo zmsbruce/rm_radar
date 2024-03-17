23dcf529c13587f7888078908d89b594a9434e9c
zmsbruce <zmsbruce@163.com>
2024-03-17 14:38:00 +0800

    refactor(samples): :art: alternate removed Detection::cv_rect()

--- samples/sample_detect_image.cpp
+++ samples/sample_detect_image.cpp
@@ -8,6 +8,11 @@
 
 using namespace radar;
 
+inline cv::Rect2f cv_rect(const Detection& detection) {
+    return cv::Rect2f(detection.x, detection.y, detection.width,
+                      detection.height);
+}
+
 int main() {
     std::unique_ptr<Detector> car_detector{nullptr}, armor_detector{nullptr};
     car_detector = std::make_unique<Detector>("../models/car.engine", 1, 1);
@@ -24,18 +29,18 @@ int main() {
     car_images.reserve(car_detections.size());
     std::for_each(car_detections.begin(), car_detections.end(),
                   [&](const Detection& detection) {
-                      car_images.push_back(image(detection.cv_rect()).clone());
+                      car_images.push_back(image(cv_rect(detection)).clone());
                   });
     armor_detections_batch = armor_detector->detect(car_images);
 
     for (size_t i = 0; i < car_detections.size(); ++i) {
         const auto& car_detection{car_detections[i]};
-        auto car_rect{car_detection.cv_rect()};
+        auto car_rect{cv_rect(car_detection)};
 
         cv::rectangle(image, car_rect, cv::Scalar(0, 255, 0));
         const auto& armor_detections{armor_detections_batch[i]};
         for (const auto& detection : armor_detections) {
-            auto armor_rect = detection.cv_rect();
+            auto armor_rect = cv_rect(detection);
             armor_rect.x += car_rect.x;
             armor_rect.y += car_rect.y;
 

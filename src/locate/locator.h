9e04fe0292b4ebb637fe01ce1967e509754f2593
zmsbruce <zmsbruce@163.com>
2024-03-26 18:09:34 +0800

    perf(locator): :zap: use zooming and scaling to increase speed and accuracy

--- src/locate/locator.h
+++ src/locate/locator.h
@@ -2,6 +2,7 @@
 #include <pcl/point_types.h>
 #include <pcl/segmentation/extract_clusters.h>
 
+#include <deque>
 #include <functional>
 #include <opencv2/opencv.hpp>
 #include <unordered_map>
@@ -26,23 +27,28 @@ class Locator {
 
     Locator(int image_width, int image_height, const cv::Matx33f& intrinsic,
             const cv::Matx44f& lidar_to_camera,
-            const cv::Matx44f& world_to_camera, float min_depth_diff = 500,
-            float max_depth_diff = 4000, float tolerance = 200.f,
-            float min_cluster_point_num = 10,
+            const cv::Matx44f& world_to_camera, float zoom_factor = 0.5f,
+            float scale_factor = 1.5, size_t queue_size = 3,
+            float min_depth_diff = 500, float max_depth_diff = 4000,
+            float tolerance = 200.f, float min_cluster_point_num = 10,
             float max_cluster_point_num = 5000, float max_distance = 29300);
 
-    void update(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr) noexcept;
+    void update(const pcl::PointCloud<pcl::PointXYZ>& cloud) noexcept;
 
     void cluster() noexcept;
 
-    void search(std::vector<Robot>& robot) noexcept;
+    void search(std::vector<Robot>& robot) const noexcept;
 
    private:
     cv::Point3f cameraToLidar(const cv::Point3f& point) const noexcept;
     cv::Point3f lidarToWorld(const cv::Point3f& point) const noexcept;
     cv::Point3f lidarToCamera(const cv::Point3f& point) const noexcept;
-    void search(Robot& robot) noexcept;
+    void search(Robot& robot) const noexcept;
+    cv::Rect zoomAndScale(const cv::Rect& rect) const noexcept;
     int image_width_, image_height_;
+    float zoom_factor_, scale_factor_;
+    int image_width_zoomed_, image_height_zoomed_;
+    size_t queue_size_;
     cv::Matx33f intrinsic_, intrinsic_inv_;
     cv::Matx44f lidar_to_camera_transform_;
     cv::Matx31f camera_to_lidar_translate_;
@@ -51,6 +57,7 @@ class Locator {
     float min_depth_diff_, max_depth_diff_;
     float max_distance_;
     cv::Mat depth_image_, background_depth_image_, diff_depth_image_;
+    std::deque<cv::Mat> depth_images_;
     pcl::PointCloud<pcl::PointXYZ>::Ptr diff_cloud_{nullptr};
     pcl::search::KdTree<pcl::PointXYZ>::Ptr search_tree_{nullptr};
     pcl::EuclideanClusterExtraction<pcl::PointXYZ> extraction_;

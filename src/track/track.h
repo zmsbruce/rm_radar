768974d98f80199bcc6ab9b3c014e168dfcc3b03
zmsbruce <zmsbruce@163.com>
2024-04-09 18:25:44 +0800

    feat(tracker): :sparkles: update class `Track`

--- src/track/track.h
+++ src/track/track.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <chrono>
+#include <cmath>
+#include <memory>
+#include <opencv2/opencv.hpp>
+
+#include "features.h"
+#include "singer.h"
 
 namespace radar::track {
 
@@ -8,9 +15,34 @@ enum class TrackState { Tentative, Confirmed, Deleted };
 
 class Track {
    public:
-    Track(const Eigen::VectorXf& mean, const Eigen::MatrixXf& covariance,
-          const Eigen::VectorXf& feature, int track_id, int n_init,
-          int max_age);
+    using TimePoint = std::chrono::high_resolution_clock::time_point;
+
+    Track(const cv::Point3f& location, const Eigen::VectorXf& feature,
+          const TimePoint& time, int track_id, int init_thresh, int miss_thresh,
+          float max_accelaration, float accelaration_correlation_time,
+          const cv::Point3f& observation_noise)
+        : features_{feature.size(), 1},
+          timestamp_{time},
+          track_id_{track_id},
+          init_count_{0},
+          miss_count_{0},
+          init_thresh_{init_thresh},
+          miss_thresh_{miss_thresh},
+          state_{TrackState::Tentative} {
+        features_.col(0) = feature;
+
+        const Eigen::Matrix<float, kStateSize, 1> initial_state =
+            Eigen::Matrix<float, kStateSize, 1>::Zero();
+        const Eigen::Matrix<float, kStateSize, kStateSize> initial_covariance =
+            Eigen::Matrix<float, kStateSize, kStateSize>::Identity() * 0.1f;
+        Eigen::Matrix<float, kMeasurementSize, kMeasurementSize>
+            observation_noise_mat;
+        observation_noise_mat << observation_noise.x, observation_noise.y,
+            observation_noise.z;
+        ekf_ = std::make_unique<SingerEKF>(
+            initial_state, initial_covariance, max_accelaration,
+            accelaration_correlation_time, observation_noise_mat);
+    }
 
     inline bool isConfirmed() const noexcept {
         return state_ == TrackState::Confirmed;
@@ -26,39 +58,59 @@ class Track {
 
     inline void setTrack(TrackState state) noexcept { state_ = state; }
 
-    inline void updateFeature(const Eigen::VectorXf& feature) {
+    void predict(const TimePoint& current_timestamp) {
+        // update ekf
+        float dt = static_cast<float>(
+                       std::chrono::duration_cast<std::chrono::nanoseconds>(
+                           current_timestamp - timestamp_)
+                           .count()) /
+                   1e9;
+        ekf_->predict(dt);
+
+        // update counter and state
+        if (isConfirmed()) {
+            miss_count_ += 1;
+            if (miss_count_ >= miss_thresh_) {
+                state_ = TrackState::Deleted;
+            }
+        }
+
+        // update timestamp
+        timestamp_ = current_timestamp;
+    }
+
+    void update(const cv::Point3f& location, const Eigen::VectorXf& feature) {
+        // update feature
         features_.conservativeResize(features_.rows(), features_.cols() + 1);
         features_.col(features_.cols() - 1) = feature;
+
+        Eigen::Matrix<float, kMeasurementSize, 1> measurement;
+        measurement << location.x, location.y, location.z;
+        ekf_->update(measurement);
+
+        // update counter and state
+        if (isTentative()) {
+            init_count_ += 1;
+            if (init_count_ >= init_thresh_) {
+                state_ = TrackState::Confirmed;
+            }
+        } else if (isConfirmed()) {
+            miss_count_ = 0;
+        }
     }
 
    private:
     Track() = delete;
-    Eigen::VectorXf mean_;
-    Eigen::MatrixXf covariance_;
-    Eigen::MatrixXf features_;
-    int track_id_;
-    int n_init_;
-    int max_age_;
-    int hits_;
-    int age_;
-    int time_since_update_;
-    TrackState state_;
-};
 
-Track::Track(const Eigen::VectorXf& mean, const Eigen::MatrixXf& covariance,
-             const Eigen::VectorXf& feature, int track_id, int n_init,
-             int max_age)
-    : mean_{mean},
-      covariance_{covariance},
-      features_{feature.size(), 1},
-      track_id_{track_id},
-      n_init_{n_init},
-      max_age_{max_age},
-      hits_{1},
-      age_{1},
-      time_since_update_{0},
-      state_{TrackState::Tentative} {
-    features_.col(0) = feature;
-}
+    Eigen::MatrixXf features_;
+    TimePoint timestamp_;
+    int track_id_;
+    int init_count_;
+    int miss_count_;
+    const int init_thresh_;
+    const int miss_thresh_;
+    TrackState state_;
+    std::unique_ptr<SingerEKF> ekf_;
+};
 
 }  // namespace radar::track

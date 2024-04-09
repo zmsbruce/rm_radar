1e9d1395ca556039a2e8c789fb0a73c36050a497
zmsbruce <zmsbruce@163.com>
2024-04-09 20:54:41 +0800

    style(tracker): :bulb: update comments

--- src/track/track.h
+++ src/track/track.h
@@ -1,3 +1,14 @@
+/**
+ * @file track.h
+ * @author zmsbruce (zmsbruce@163.com)
+ * @brief This file implements a Track class and its functions.
+ * @date 2024-04-09
+ *
+ * @copyright (c) 2024 HITCRT
+ * All rights reserved.
+ *
+ */
+
 #pragma once
 
 #include <Eigen/Dense>
@@ -11,13 +22,43 @@
 
 namespace radar::track {
 
+/**
+ * @brief Enum representing the state of the track.
+ *
+ */
+enum class TrackState { Tentative, Confirmed, Deleted };
+
+/**
+ * @class Track
+ * @brief Represents a track in a tracking system.
+ *
+ * This class encapsulates a track which is defined by its state (tentative,
+ * confirmed, deleted), a Kalman filter for state estimation, and various
+ * counters and thresholds for track management.
+ */
 class Track {
    public:
-    enum class TrackState { Tentative, Confirmed, Deleted };
-    using TimePoint = std::chrono::high_resolution_clock::time_point;
-
+    /**
+     * @brief Constructs a Track object with the given initial parameters.
+     *
+     * @param location Initial location of the track.
+     * @param feature Initial feature vector associated with the track.
+     * @param time Initial timestamp of the track.
+     * @param track_id Unique identifier for the track.
+     * @param init_thresh Threshold for the number of consecutive detections to
+     * confirm the track.
+     * @param miss_thresh Threshold for the number of missed detections to
+     * delete the track.
+     * @param max_accelaration Maximum acceleration used in the motion model of
+     * the Kalman filter.
+     * @param accelaration_correlation_time Correlation time of the acceleration
+     * used in the motion model.
+     * @param observation_noise Observation noise for the Kalman filter
+     * measurements.
+     */
     Track(const cv::Point3f& location, const Eigen::VectorXf& feature,
-          const TimePoint& time, int track_id, int init_thresh, int miss_thresh,
+          const std::chrono::high_resolution_clock::time_point& time,
+          int track_id, int init_thresh, int miss_thresh,
           float max_accelaration, float accelaration_correlation_time,
           const cv::Point3f& observation_noise)
         : features_{feature},
@@ -41,21 +82,43 @@ class Track {
             accelaration_correlation_time, observation_noise_mat);
     }
 
+    /**
+     * @brief Determines if the track is confirmed.
+     * @return True if the track is confirmed, false otherwise.
+     */
     inline bool isConfirmed() const noexcept {
         return state_ == TrackState::Confirmed;
     }
 
+    /**
+     * @brief Determines if the track is tentative.
+     * @return True if the track is tentative, false otherwise.
+     */
     inline bool isTentative() const noexcept {
         return state_ == TrackState::Tentative;
     }
 
+    /**
+     * @brief Determines if the track is deleted.
+     * @return True if the track is deleted, false otherwise.
+     */
     inline bool isDeleted() const noexcept {
         return state_ == TrackState::Deleted;
     }
 
+    /**
+     * @brief Sets the current state of the track.
+     * @param state The new state to set for the track.
+     */
     inline void setTrackState(TrackState state) noexcept { state_ = state; }
 
-    void predict(const TimePoint& current_timestamp) {
+    /**
+     * @brief Predicts the next state of the track using the Kalman filter.
+     * @param current_timestamp The current timestamp to which the prediction is
+     * made.
+     */
+    void predict(const std::chrono::high_resolution_clock::time_point&
+                     current_timestamp) {
         // update ekf
         float dt = static_cast<float>(
                        std::chrono::duration_cast<std::chrono::nanoseconds>(
@@ -76,6 +139,11 @@ class Track {
         timestamp_ = current_timestamp;
     }
 
+    /**
+     * @brief Updates the track with a new observation.
+     * @param location The new observed location of the track.
+     * @param feature The new observed feature associated with the track.
+     */
     void update(const cv::Point3f& location, const Eigen::VectorXf& feature) {
         // update feature
         features_.push_back(feature);
@@ -98,7 +166,7 @@ class Track {
     Track() = delete;
 
     Features features_;
-    TimePoint timestamp_;
+    std::chrono::high_resolution_clock::time_point timestamp_;
     int track_id_;
     int init_count_;
     int miss_count_;

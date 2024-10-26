d62c4f500a7128535e91233681cdd11fe3df2fe2
TianranW <529708894@qq.com>
2024-10-26 21:45:30 +0800

    build(driver): :bug: fix finding Hikvision

--- cmake/FindHikvision.cmake
+++ cmake/FindHikvision.cmake
@@ -9,16 +9,13 @@ set(lib_names FormatConversion
 set(path_names /opt/MVS/lib/64 /opt/MVS/lib/32 /opt/MVS/lib)
 
 foreach(lib_name IN LISTS lib_names)
-    unset(Hikvision_LIB)  
-  
+    unset(Hikvision_LIB CACHE)
     find_library(Hikvision_LIB NAMES ${lib_name} PATHS ${path_names})
 
     if(Hikvision_LIB)
+        message(STATUS "Hikvision found: ${Hikvision_LIB}")
         list(APPEND Hikvision_LIBS ${Hikvision_LIB})
-        if(NOT hikvision_find_quietly)
-            message(STATUS "Hikvision found: ${Hikvision_LIB}")
-        endif()
-    elseif(hikvision_find_required)
+    else()
         message(FATAL_ERROR "Could not find Hikvision library: ${lib_name}")
     endif()
 endforeach()

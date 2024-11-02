2db5b37897e45474106d82400c89e5ec498d8375
zmsbruce <zmsbruce@163.com>
2024-11-02 22:43:18 +0800

    feat(driver): :sparkles: update serial port

---
+++ src/driver/serial/serial.cpp
@@ -0,0 +1,157 @@
+/**
+ * @file serial.cpp
+ * @author zmsbruce (zmsbruce@163.com)
+ * @brief This file contains the implementation of the `Serial` class, which
+ * provides methods to manage serial port operations such as opening, closing,
+ * reading, and writing. The class ensures thread safety via mutex locks to
+ * prevent concurrent access issues.
+ * @date 2024-11-02
+ *
+ * @copyright (c) 2024 HITCRT
+ * All rights reserved.
+ *
+ */
+
+#include "serial.h"
+
+#include <spdlog/spdlog.h>
+
+#include <magic_enum/magic_enum.hpp>
+
+namespace radar {
+
+Serial::Serial(std::string_view device_name, LibSerial::BaudRate baud_rate)
+    : device_name_(device_name), baud_rate_(baud_rate) {
+    spdlog::debug("Serial parameters: device name: {}, baud rate: {}",
+                  device_name_, magic_enum::enum_name(baud_rate_));
+    spdlog::trace("Serial {} initialized.", device_name_);
+}
+
+Serial::~Serial() {
+    spdlog::trace("Destroying serial {}", device_name_);
+
+    // Lock to ensure thread safety during destruction.
+    std::lock_guard<std::mutex> lock(serial_mutex_);
+    spdlog::debug("Serial {} lock acquired.", device_name_);
+
+    // Check if the serial port is still open and attempt to close it.
+    if (isOpen()) {
+        spdlog::debug("Serial {} is still open, attempting to close it.",
+                      device_name_);
+        try {
+            close();  // Close the serial port if still open.
+        } catch (...) {
+            spdlog::error(
+                "Unknown exception caught while closing serial {} during "
+                "destruction.",
+                device_name_);
+        }
+    }
+    spdlog::trace("Serial {} deconstructed.", device_name_);
+}
+
+inline bool Serial::isOpen() const noexcept { return is_open_; }
+
+bool Serial::open() {
+    std::lock_guard<std::mutex> lock(serial_mutex_);
+    spdlog::debug("Serial {} lock acquired.", device_name_);
+
+    // Check if the serial port is already open.
+    if (isOpen()) {
+        spdlog::error("Serial {} is already open.", device_name_);
+        return false;
+    }
+
+    try {
+        // Open the serial port and set the baud rate.
+        serial_port_.Open(device_name_);
+        serial_port_.SetBaudRate(baud_rate_);
+
+        is_open_ = true;  // Mark the serial port as open.
+        spdlog::info("Serial {} opened with baud rate: {}.", device_name_,
+                     magic_enum::enum_name(baud_rate_));
+        return true;
+    } catch (const LibSerial::OpenFailed& err) {
+        spdlog::error("Failed to open serial {}: {}", device_name_, err.what());
+        return false;
+    }
+}
+
+void Serial::close() {
+    std::lock_guard<std::mutex> lock(serial_mutex_);
+    spdlog::debug("Serial {} lock acquired.", device_name_);
+
+    // Check if the serial port is already closed.
+    if (!isOpen()) {
+        spdlog::error("Serial {} is not open.", device_name_);
+        return;
+    }
+
+    try {
+        // Close the serial port and mark as not open.
+        serial_port_.Close();
+        is_open_ = false;
+
+        spdlog::info("Serial {} closed.", device_name_);
+    } catch (const std::exception& err) {
+        spdlog::error("Failed to close serial {}: {}", device_name_,
+                      err.what());
+    }
+}
+
+bool Serial::read(std::span<std::byte> buffer) {
+    if (!isOpen()) {
+        spdlog::error("Attempted to read from closed serial {}", device_name_);
+        return false;
+    }
+
+    try {
+        std::lock_guard<std::mutex> lock(serial_mutex_);
+        spdlog::debug("Serial {} lock acquired.", device_name_);
+
+        spdlog::trace("Serial {} will read with buffer size: {}", device_name_,
+                      buffer.size());
+
+        // Prepare the buffer for reading data from the serial port.
+        LibSerial::DataBuffer data_buffer(buffer.data(),
+                                          buffer.data() + buffer.size());
+
+        serial_port_.Read(data_buffer, buffer.size());  // Read into the buffer.
+        spdlog::debug("Buffer read successfully from serial {}", device_name_);
+
+        return true;
+    } catch (const std::exception& err) {
+        spdlog::error("Failed to read from serial {}: {}", device_name_,
+                      err.what());
+        return false;
+    }
+}
+
+bool Serial::write(const std::span<const std::byte> data) {
+    if (!isOpen()) {
+        spdlog::error("Attempted to write to closed serial {}", device_name_);
+        return false;
+    }
+
+    try {
+        std::lock_guard<std::mutex> lock(serial_mutex_);
+        spdlog::debug("Serial {} lock acquired.", device_name_);
+
+        spdlog::trace("Serial {} will write with data size: {}", device_name_,
+                      data.size());
+
+        // Prepare the data buffer for writing to the serial port.
+        LibSerial::DataBuffer data_buffer(data.data(),
+                                          data.data() + data.size());
+
+        serial_port_.Write(data_buffer);  // Write the data to the serial port.
+        spdlog::debug("Data wrote successfully to serial {}", device_name_);
+        return true;
+    } catch (const std::exception& err) {
+        spdlog::error("Failed to write to serial {}: {}", device_name_,
+                      err.what());
+        return false;
+    }
+}
+
+}  // namespace radar
---
+++ src/driver/serial/serial.h
@@ -0,0 +1,105 @@
+/**
+ * @file serial.h
+ * @author zmsbruce (zmsbruce@163.com)
+ * @brief This file declares the `Serial` class, which provides an abstraction
+ * for managing serial port operations like opening, closing, reading, and
+ * writing. The class ensures thread safety and allows for easy integration with
+ * different baud rates.
+ * @date 2024-11-02
+ *
+ * @copyright (c) 2024 HITCRT
+ * All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include <libserial/SerialPort.h>
+
+#include <atomic>
+#include <mutex>
+#include <span>
+#include <string>
+#include <string_view>
+
+namespace radar {
+
+/**
+ * @brief Serial class encapsulates the functionality for handling serial port
+ *        operations such as open, close, read, and write.
+ */
+class Serial {
+   public:
+    /**
+     * @brief Constructor for the Serial class.
+     *
+     * @param device_name The name of the serial device (e.g., "/dev/ttyUSB0").
+     * @param baud_rate The baud rate for the serial communication (default is
+     * 115200).
+     */
+    Serial(std::string_view device_name,
+           LibSerial::BaudRate baud_rate = LibSerial::BaudRate::BAUD_115200);
+
+    /**
+     * @brief Destructor for the Serial class. Ensures the serial port is
+     * closed.
+     */
+    ~Serial();
+
+    /**
+     * @brief Check if the serial port is open.
+     *
+     * @return true if the serial port is open, false otherwise.
+     */
+    inline bool isOpen() const noexcept;
+
+    /**
+     * @brief Opens the serial port with the specified device name and baud
+     * rate.
+     *
+     * @return true if the serial port was successfully opened, false otherwise.
+     */
+    bool open();
+
+    /**
+     * @brief Closes the serial port if it is open.
+     */
+    void close();
+
+    /**
+     * @brief Reads data from the serial port into the provided buffer.
+     *
+     * @param buffer A span of bytes to store the read data.
+     * @return true if data was successfully read, false otherwise.
+     */
+    bool read(std::span<std::byte> buffer);
+
+    /**
+     * @brief Writes data to the serial port.
+     *
+     * @param data A span of bytes containing the data to write.
+     * @return true if data was successfully written, false otherwise.
+     */
+    bool write(const std::span<const std::byte> data);
+
+   private:
+    // Disable default constructor
+    Serial() = delete;
+
+    /// The serial port object from the LibSerial library.
+    LibSerial::SerialPort serial_port_;
+
+    /// The name of the serial device (e.g., "/dev/ttyUSB0").
+    std::string device_name_;
+
+    /// The baud rate for the serial communication.
+    LibSerial::BaudRate baud_rate_;
+
+    /// Flag indicating if the serial port is open.
+    std::atomic_bool is_open_ = false;
+
+    /// Mutex to ensure thread safety for serial port operations.
+    std::mutex serial_mutex_;
+};
+
+}  // namespace radar

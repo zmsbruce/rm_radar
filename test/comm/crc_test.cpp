541e48dcf96813382ed79e646d1c740a2dec42e9
TianranW <529708894@qq.com>
2024-11-17 17:04:34 +0800

    build(third-party): :recycle: remove dependency FastCRC

--- test/comm/crc_test.cpp
+++ test/comm/crc_test.cpp
@@ -12,23 +12,17 @@
 #undef protected
 
 class CrcTest : public ::testing::Test {
-   private:
-   protected:
-    std::unique_ptr<radar::RefereeCommunicator> comm = nullptr;
-
-    virtual void SetUp() {
-        comm = std::make_unique<radar::RefereeCommunicator>("/dev/ttyUSB0");
-    }
+    virtual void SetUp() {}
 };
 
 TEST_F(CrcTest, TestCRC8) {
     std::vector<std::byte> data = {std::byte(0x01), std::byte(0x02),
                                    std::byte(0x03), std::byte(0x00)};
     radar::RefereeCommunicator::appendCRC8(data);
-    EXPECT_EQ(data[3], std::byte(0x48));
+    EXPECT_EQ(data[3], std::byte(0x0A));
 
     std::vector<std::byte> dataToVerify = {std::byte(0x01), std::byte(0x02),
-                                           std::byte(0x03), std::byte(0x48)};
+                                           std::byte(0x03), std::byte(0x0A)};
     EXPECT_TRUE(radar::RefereeCommunicator::verifyCRC8(dataToVerify));
 }
 
